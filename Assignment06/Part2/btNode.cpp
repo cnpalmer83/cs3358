e527a13a3fb1df182e98b164fcfc75d9011df241
Chad Palmer <chad.n.palmer@gmail.com>
2018-10-30 20:47:34 -0500

    part 2 of assignment 6 complete

--- Assignment06/Part2/btNode.cpp
+++ Assignment06/Part2/btNode.cpp
@@ -33,8 +33,8 @@ int bst_size(btNode* bst_root)
 // write definition for bst_insert here
 void bst_insert(btNode*& bst_root, const int& insInt)
 {
-   if (bst_root == 0)
-   {
+   if (bst_root == 0)                                       // Condition 1: Tree is empty, insInt
+   {                                                        //              assigned to root.
       btNode* newNodePtr = new btNode;
       newNodePtr->data   = insInt;
       newNodePtr->left   = 0;
@@ -42,14 +42,14 @@ void bst_insert(btNode*& bst_root, const int& insInt)
       bst_root = newNodePtr;
       return;
    }
-   else
+   else                                                     // Case for non-empty tree.
    {
       btNode* cur = bst_root;
-      while (true)
+      while (true)                                          // Traverse until insert point found.
       {
-         if (insInt < cur->data)
+         if (insInt < cur->data)                            // insInt < data, check for insert condition.
          {
-            if (cur->left == 0)
+            if (cur->left == 0)                             // If left child vacant, fill with new node.
             {
                cur->left = new btNode;
                cur->left->data = insInt;
@@ -57,13 +57,13 @@ void bst_insert(btNode*& bst_root, const int& insInt)
                cur->left->right = 0;
                return;
             }
-            else
+            else                                            // Else, traverse left.
                cur = cur->left;
          }
 
-         else if (insInt > cur->data)
+         else if (insInt > cur->data)                       // insInt > data, check for insert condition.
          {
-            if (cur->right == 0)
+            if (cur->right == 0)                            // If right child vacant, fill with new node.
             {
                cur->right = new btNode;
                cur->right->data = insInt;
@@ -71,13 +71,13 @@ void bst_insert(btNode*& bst_root, const int& insInt)
                cur->right->right = 0;
                return;
             }
-            else
+            else                                            // Else, traverse right.
                cur = cur->right;
          }
 
-         else //(insInt == cur->data)
+         else //(insInt == cur->data)                       // insInt == data implied
          {
-            cur->data = insInt;
+            cur->data = insInt;                             // Overwrite existing data with insInt.
             return;
          }
       }
@@ -86,26 +86,26 @@ void bst_insert(btNode*& bst_root, const int& insInt)
 
 bool bst_remove(btNode*& bst_root, const int& remInt)
 {
-   if (bst_root == 0)
+   if (bst_root == 0)                                       // Base case (tree is empty)
       return false;
-   else if (remInt < bst_root->data)
+   else if (remInt < bst_root->data)                        // remInt < data (traverse left)
       return bst_remove(bst_root->left, remInt);
-   else if (remInt > bst_root->data)
+   else if (remInt > bst_root->data)                        // remInt > data (traverse right)
       return bst_remove(bst_root->right, remInt);
-   else if (bst_root->left == 0 || bst_root->right == 0)
+   else if (bst_root->left == 0 || bst_root->right == 0)    // remInt found and 0 or 1 child exists
    {
       btNode* temp = bst_root;
-      if (bst_root->left != 0)
+      if (bst_root->left != 0)                              // Case for left child
          bst_root = bst_root->left;
-      else if (bst_root->right != 0)
+      else if (bst_root->right != 0)                        // Case for right child
          bst_root = bst_root->right;
-      else
+      else                                                  // Case for no children
          bst_root = 0;
 
       delete temp;
       return true;
    }
-   else
+   else                                                     // Case for two children
    {
       bst_remove_max(bst_root->left, bst_root->data);
       return true;
@@ -114,16 +114,16 @@ bool bst_remove(btNode*& bst_root, const int& remInt)
 
 void bst_remove_max(btNode*& bst_root, int& removed)
 {
-   if (bst_root->right == 0)
+   if (bst_root->right == 0)                                // Base case (max value of tree found)
    {
-      if (bst_root->left != 0)
+      if (bst_root->left != 0)                              // Condition 1: max value has left child
       {
          btNode* temp = bst_root;
          removed = bst_root->data;
          bst_root = bst_root->left;
          delete temp;
       }
-      else
+      else                                                  // Condition 2: max value has no children
       {
          removed = bst_root->data;
          delete bst_root;
@@ -131,6 +131,6 @@ void bst_remove_max(btNode*& bst_root, int& removed)
       }
       return;
    }
-   else
+   else                                                     // Recursive search for max until found
       bst_remove_max(bst_root->right, removed);
 }
